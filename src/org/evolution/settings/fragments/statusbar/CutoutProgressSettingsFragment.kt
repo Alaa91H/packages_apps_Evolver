418135766fd24c9252e1a2b59bfa696c79814543
Alaa <143001551+Alaa91H@users.noreply.github.com>
2026-09-20 20:50:20 +0200

    Evolver: add Aurora and timer ring preferences

--- src/org/evolution/settings/fragments/statusbar/CutoutProgressSettingsFragment.kt
+++ src/org/evolution/settings/fragments/statusbar/CutoutProgressSettingsFragment.kt
@@ -78,6 +78,16 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         private const val KEY_MUSIC_WAVE_AMPLITUDE = "cutout_progress_music_wave_amplitude_dp10"
         private const val KEY_MUSIC_WAVE_DENSITY = "cutout_progress_music_wave_density"
         private const val KEY_MUSIC_WAVE_SPEED = "cutout_progress_music_wave_speed"
+        private const val KEY_TIMER_PRESENTATION = "cutout_progress_timer_presentation"
+        private const val KEY_TIMER_COLOR_MODE = "cutout_progress_timer_color_mode"
+        private const val KEY_TIMER_CUSTOM_COLOR = "cutout_progress_timer_custom_color"
+        private const val KEY_AURORA_COLOR_MODE = "cutout_progress_aurora_color_mode"
+        private const val KEY_AURORA_CUSTOM_COLOR = "cutout_progress_aurora_custom_color"
+        private const val KEY_AURORA_NOTIFICATION_COLOR_MODE =
+            "cutout_progress_aurora_notification_color_mode"
+
+        private const val AURORA_COLOR_MODE_SPECTRUM = 0
+        private const val AURORA_COLOR_MODE_CUSTOM = 2
 
         private const val DEFAULT_COMPLETION_PULSE = 1
         private const val DEFAULT_AUTO_GEOMETRY = 1
@@ -94,12 +104,18 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         private const val DEFAULT_MUSIC_WAVE_AMPLITUDE = 25
         private const val DEFAULT_MUSIC_WAVE_DENSITY = 48
         private const val DEFAULT_MUSIC_WAVE_SPEED = 100
+        private const val DEFAULT_TIMER_PRESENTATION = 0
+        private const val DEFAULT_TIMER_COLOR_MODE = 0
+        private const val DEFAULT_AURORA_COLOR_MODE = 0
+        private const val DEFAULT_AURORA_NOTIFICATION_COLOR_MODE = 0
 
         private const val DEFAULT_RING_COLOR = 0xFF2196F3.toInt()
         private const val DEFAULT_ERROR_COLOR = 0xFFF44336.toInt()
         private const val DEFAULT_FLASH_COLOR = 0xFFFFFFFF.toInt()
         private const val DEFAULT_BG_COLOR = 0xFF808080.toInt()
         private const val DEFAULT_MUSIC_COLOR = 0xFF9C27B0.toInt()
+        private const val DEFAULT_TIMER_COLOR = 0xFFFF8A00.toInt()
+        private const val DEFAULT_AURORA_COLOR = 0xFF7C4DFF.toInt()
 
         @JvmStatic
         fun ensureCalibratedDefaults(resolver: ContentResolver) {
@@ -118,7 +134,11 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
                 KEY_MUSIC_WAVE_ENABLED to DEFAULT_MUSIC_WAVE_ENABLED,
                 KEY_MUSIC_WAVE_AMPLITUDE to DEFAULT_MUSIC_WAVE_AMPLITUDE,
                 KEY_MUSIC_WAVE_DENSITY to DEFAULT_MUSIC_WAVE_DENSITY,
-                KEY_MUSIC_WAVE_SPEED to DEFAULT_MUSIC_WAVE_SPEED
+                KEY_MUSIC_WAVE_SPEED to DEFAULT_MUSIC_WAVE_SPEED,
+                KEY_TIMER_PRESENTATION to DEFAULT_TIMER_PRESENTATION,
+                KEY_TIMER_COLOR_MODE to DEFAULT_TIMER_COLOR_MODE,
+                KEY_AURORA_COLOR_MODE to DEFAULT_AURORA_COLOR_MODE,
+                KEY_AURORA_NOTIFICATION_COLOR_MODE to DEFAULT_AURORA_NOTIFICATION_COLOR_MODE
             )
 
             defaults.forEach { (key, value) ->
@@ -135,8 +155,12 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
 
     private lateinit var ringColorModePref: ListPreference
     private lateinit var musicColorModePref: ListPreference
+    private lateinit var timerColorModePref: ListPreference
+    private lateinit var auroraColorModePref: ListPreference
+    private lateinit var auroraNotificationColorModePref: ListPreference
     private lateinit var downloadPresentationPref: ListPreference
     private lateinit var musicPresentationPref: ListPreference
+    private lateinit var timerPresentationPref: ListPreference
     private lateinit var primaryPriorityPref: ListPreference
     private lateinit var multiRingSpacingPref: Preference
     private lateinit var autoGeometryPref: Preference
@@ -152,6 +176,8 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
     private lateinit var flashColorPref: Preference
     private lateinit var bgColorPref: Preference
     private lateinit var musicColorPref: Preference
+    private lateinit var timerColorPref: Preference
+    private lateinit var auroraColorPref: Preference
 
     private lateinit var finishStylePref: ListPreference
     private lateinit var easingPref: ListPreference
@@ -165,8 +191,12 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
 
         ringColorModePref = findPreference(KEY_RING_COLOR_MODE)!!
         musicColorModePref = findPreference(KEY_MUSIC_COLOR_MODE)!!
+        timerColorModePref = findPreference(KEY_TIMER_COLOR_MODE)!!
+        auroraColorModePref = findPreference(KEY_AURORA_COLOR_MODE)!!
+        auroraNotificationColorModePref = findPreference(KEY_AURORA_NOTIFICATION_COLOR_MODE)!!
         downloadPresentationPref = findPreference(KEY_DOWNLOAD_PRESENTATION)!!
         musicPresentationPref = findPreference(KEY_MUSIC_PRESENTATION)!!
+        timerPresentationPref = findPreference(KEY_TIMER_PRESENTATION)!!
         primaryPriorityPref = findPreference(KEY_PRIMARY_PRIORITY)!!
         multiRingSpacingPref = findPreference(KEY_MULTI_RING_SPACING)!!
         autoGeometryPref = findPreference(KEY_AUTO_GEOMETRY)!!
@@ -182,6 +212,8 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         flashColorPref = findPreference(KEY_FLASH_COLOR)!!
         bgColorPref = findPreference(KEY_BG_COLOR)!!
         musicColorPref = findPreference(KEY_MUSIC_CUSTOM_COLOR)!!
+        timerColorPref = findPreference(KEY_TIMER_CUSTOM_COLOR)!!
+        auroraColorPref = findPreference(KEY_AURORA_CUSTOM_COLOR)!!
 
         finishStylePref = findPreference(KEY_FINISH_STYLE)!!
         easingPref = findPreference(KEY_EASING)!!
@@ -199,9 +231,19 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         val storedMusicMode = readSecureInt(KEY_MUSIC_COLOR_MODE, MUSIC_COLOR_MODE_ALBUM_ICON)
         musicColorModePref.value = storedMusicMode.toString()
         updateColorPickerVisibility(storedMusicMode, KEY_MUSIC_COLOR_MODE)
+
+        val storedTimerMode = readSecureInt(KEY_TIMER_COLOR_MODE, DEFAULT_TIMER_COLOR_MODE)
+        timerColorModePref.value = storedTimerMode.toString()
+        updateColorPickerVisibility(storedTimerMode, KEY_TIMER_COLOR_MODE)
+
+        val storedAuroraMode = readSecureInt(KEY_AURORA_COLOR_MODE, DEFAULT_AURORA_COLOR_MODE)
+        auroraColorModePref.value = storedAuroraMode.toString()
+        updateColorPickerVisibility(storedAuroraMode, KEY_AURORA_COLOR_MODE)
+
         updateLayerPreferenceVisibility(
             readSecureInt(KEY_DOWNLOAD_PRESENTATION, DEFAULT_DOWNLOAD_PRESENTATION),
-            readSecureInt(KEY_MUSIC_PRESENTATION, DEFAULT_MUSIC_PRESENTATION)
+            readSecureInt(KEY_MUSIC_PRESENTATION, DEFAULT_MUSIC_PRESENTATION),
+            readSecureInt(KEY_TIMER_PRESENTATION, DEFAULT_TIMER_PRESENTATION)
         )
         updateManualGeometryVisibility(
             readSecureInt(KEY_AUTO_GEOMETRY, DEFAULT_AUTO_GEOMETRY) != 0
@@ -209,8 +251,12 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
 
         ringColorModePref.onPreferenceChangeListener = this
         musicColorModePref.onPreferenceChangeListener = this
+        timerColorModePref.onPreferenceChangeListener = this
+        auroraColorModePref.onPreferenceChangeListener = this
+        auroraNotificationColorModePref.onPreferenceChangeListener = this
         downloadPresentationPref.onPreferenceChangeListener = this
         musicPresentationPref.onPreferenceChangeListener = this
+        timerPresentationPref.onPreferenceChangeListener = this
         autoGeometryPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, value ->
             updateManualGeometryVisibility(value as Boolean)
             true
@@ -256,6 +302,22 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
             )
             true
         }
+        timerColorPref.setOnPreferenceClickListener {
+            showColorPicker(
+                title = getString(R.string.cutout_progress_timer_custom_color_title),
+                key = KEY_TIMER_CUSTOM_COLOR,
+                default = DEFAULT_TIMER_COLOR
+            )
+            true
+        }
+        auroraColorPref.setOnPreferenceClickListener {
+            showColorPicker(
+                title = getString(R.string.cutout_progress_aurora_custom_color_title),
+                key = KEY_AURORA_CUSTOM_COLOR,
+                default = DEFAULT_AURORA_COLOR
+            )
+            true
+        }
 
         finishStylePref.onPreferenceChangeListener = this
         easingPref.onPreferenceChangeListener = this
@@ -267,18 +329,24 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
     override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
         val intValue = (newValue as? String)?.toIntOrNull() ?: return false
 
-        if (preference.key == KEY_RING_COLOR_MODE || preference.key == KEY_MUSIC_COLOR_MODE) {
+        if (preference.key == KEY_RING_COLOR_MODE
+            || preference.key == KEY_MUSIC_COLOR_MODE
+            || preference.key == KEY_TIMER_COLOR_MODE
+            || preference.key == KEY_AURORA_COLOR_MODE) {
             updateColorPickerVisibility(intValue, preference.key)
             return true
         }
 
         if (preference.key == KEY_DOWNLOAD_PRESENTATION
-            || preference.key == KEY_MUSIC_PRESENTATION) {
+            || preference.key == KEY_MUSIC_PRESENTATION
+            || preference.key == KEY_TIMER_PRESENTATION) {
             val downloadMode = if (preference.key == KEY_DOWNLOAD_PRESENTATION) intValue
                 else readSecureInt(KEY_DOWNLOAD_PRESENTATION, DEFAULT_DOWNLOAD_PRESENTATION)
             val musicMode = if (preference.key == KEY_MUSIC_PRESENTATION) intValue
                 else readSecureInt(KEY_MUSIC_PRESENTATION, DEFAULT_MUSIC_PRESENTATION)
-            updateLayerPreferenceVisibility(downloadMode, musicMode)
+            val timerMode = if (preference.key == KEY_TIMER_PRESENTATION) intValue
+                else readSecureInt(KEY_TIMER_PRESENTATION, DEFAULT_TIMER_PRESENTATION)
+            updateLayerPreferenceVisibility(downloadMode, musicMode, timerMode)
             return true
         }
 
@@ -291,6 +359,8 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         when (key) {
             KEY_RING_COLOR_MODE -> ringColorPref.isVisible = (mode == COLOR_MODE_CUSTOM)
             KEY_MUSIC_COLOR_MODE -> musicColorPref.isVisible = (mode == MUSIC_COLOR_MODE_CUSTOM)
+            KEY_TIMER_COLOR_MODE -> timerColorPref.isVisible = (mode == COLOR_MODE_CUSTOM)
+            KEY_AURORA_COLOR_MODE -> auroraColorPref.isVisible = (mode == AURORA_COLOR_MODE_CUSTOM)
         }
     }
 
@@ -304,13 +374,16 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         ringGapPref.isVisible = showManual
     }
 
-    private fun updateLayerPreferenceVisibility(downloadMode: Int, musicMode: Int) {
-        val hasIndependentRing = downloadMode == PRESENTATION_INDEPENDENT
-            || musicMode == PRESENTATION_INDEPENDENT
-        multiRingSpacingPref.isVisible = hasIndependentRing
-        primaryPriorityPref.isVisible = downloadMode == musicMode
-            && (downloadMode == PRESENTATION_PRIMARY
-                || downloadMode == PRESENTATION_INDEPENDENT)
+    private fun updateLayerPreferenceVisibility(
+        downloadMode: Int,
+        musicMode: Int,
+        timerMode: Int
+    ) {
+        val modes = listOf(downloadMode, musicMode, timerMode)
+        multiRingSpacingPref.isVisible = modes.any { it == PRESENTATION_INDEPENDENT }
+        primaryPriorityPref.isVisible =
+            modes.count { it == PRESENTATION_PRIMARY } > 1
+                || modes.count { it == PRESENTATION_INDEPENDENT } > 1
     }
 
     private fun syncListPreferences() {
@@ -322,6 +395,10 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
             fnameTruncPref to 0,
             downloadPresentationPref to DEFAULT_DOWNLOAD_PRESENTATION,
             musicPresentationPref to DEFAULT_MUSIC_PRESENTATION,
+            timerPresentationPref to DEFAULT_TIMER_PRESENTATION,
+            timerColorModePref to DEFAULT_TIMER_COLOR_MODE,
+            auroraColorModePref to DEFAULT_AURORA_COLOR_MODE,
+            auroraNotificationColorModePref to DEFAULT_AURORA_NOTIFICATION_COLOR_MODE,
             primaryPriorityPref to DEFAULT_PRIMARY_PRIORITY
         ).forEach { (pref, default) ->
             pref.value = readSecureInt(pref.key, default).toString()
@@ -349,6 +426,8 @@ class CutoutProgressSettingsFragment : SettingsPreferenceFragment(),
         flashColorPref.summary = "#${argbToHex(readSecureInt(KEY_FLASH_COLOR, DEFAULT_FLASH_COLOR))}"
         bgColorPref.summary = "#${argbToHex(readSecureInt(KEY_BG_COLOR, DEFAULT_BG_COLOR))}"
         musicColorPref.summary = "#${argbToHex(readSecureInt(KEY_MUSIC_CUSTOM_COLOR, DEFAULT_MUSIC_COLOR))}"
+        timerColorPref.summary = "#${argbToHex(readSecureInt(KEY_TIMER_CUSTOM_COLOR, DEFAULT_TIMER_COLOR))}"
+        auroraColorPref.summary = "#${argbToHex(readSecureInt(KEY_AURORA_CUSTOM_COLOR, DEFAULT_AURORA_COLOR))}"
     }
 
     private fun readSecureInt(key: String, default: Int): Int =
